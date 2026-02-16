a1615f16c9619a92921fda29480946dc885223cf
midudev <miduga@gmail.com>
2026-02-16 17:29:11 +0100

    Add Vercel adapter for deployment and update dependencies. Include .vercel in .gitignore.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import links from './src/data/links.json';
 
+import vercel from '@astrojs/vercel';
+
 const redirects = Object.fromEntries(
   links.map(({ slug, url }) => [`/${slug}`, url])
 );
 
 export default defineConfig({
   redirects,
+  adapter: vercel(),
 });
